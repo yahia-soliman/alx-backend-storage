75eb4be4076dc69f790508495c2f556afc1badf1
Yahia <yn7599@gmail.com>
2024-07-11 01:05:38 +0300

    0x00: task 3 update

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 -- ranked by their longevity (life span untill 2022)
 SELECT band_name, split - formed as lifespan
   FROM metal_bands
- WHERE style = 'Glam rock'
+ WHERE style LIKE '%Glam rock%'
  ORDER BY lifespan DESC;
