03fc0c0bf94da38a67fdac8551d1a4184ce8193d
Yahia <yn7599@gmail.com>
2024-07-11 00:47:52 +0300

    0x00: 3-glam_rock.sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- List all bands with `Glam rock` as their main style
+-- ranked by their longevity (life span untill 2022)
+SELECT band_name, split - formed as lifespan
+  FROM metal_bands
+ WHERE style = 'Glam rock';
