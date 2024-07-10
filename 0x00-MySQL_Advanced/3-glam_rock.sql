e91f48342315d50f1710c739b303ec00f4addd23
Yahia <yn7599@gmail.com>
2024-07-11 01:31:18 +0300

    0x00: task 3 update with IF

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- List all bands with `Glam rock` as their main style
 -- ranked by their longevity (life span untill 2022)
-SELECT band_name, split - formed as lifespan
+SELECT band_name, IF(split, split - formed, 2022 - formed) as lifespan
   FROM metal_bands
  WHERE style LIKE '%Glam rock%'
  ORDER BY lifespan DESC;
