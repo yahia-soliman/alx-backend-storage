8bde85731c28c7406eaaf9610dcb3f4c47be9f93
Yahia <yn7599@gmail.com>
2024-07-18 00:44:06 +0300

    0x02: task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,11 +3,21 @@
 import redis
 
 import typing as t
+from functools import wraps
 from uuid import uuid4
 
 
 data_t = t.Union[str, bytes, int, float]
 converter_t = t.Callable
+
+
+def count_calls(method: t.Callable) -> t.Callable:
+    """Automaicaly count method's call time"""
+    @wraps(method)
+    def counter(self, *args, **kwargs):
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return counter
 
 
 class Cache:
@@ -17,13 +27,14 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: data_t) -> str:
         """Store `data` in cache"""
         id = str(uuid4())
         self._redis.set(id, data)
         return id
 
-    def get(self, key: str, fn: t.Optional[converter_t]=None):
+    def get(self, key: str, fn: t.Optional[converter_t] = None):
         """Get the value of specific key from the cache"""
         res = self._redis.get(key)
         return fn(res) if fn else res
@@ -33,3 +44,12 @@ class Cache:
 
     def get_int(self, key: str):
         return self.get(key, int)
+
+cache = Cache()
+
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
+
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
