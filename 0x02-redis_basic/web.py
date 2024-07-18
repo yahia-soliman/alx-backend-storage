eb9fe82c2fa55afd627c5a650440a63ca3e01461
Yahia <yn7599@gmail.com>
2024-07-18 14:19:15 +0300

    0x02: caching web responses

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Web caching with Redis"""
+import redis
+import requests
+
+r = redis.Redis()
+
+
+def get_page(url: str) -> str:
+    """Get a web page, cache it for 10 seconds"""
+    cached = r.get(url)
+    if isinstance(cached, bytes):
+        html = cached.decode()
+    else:
+        res = requests.get(url)
+        html = res.text
+        r.set(url, html, ex=10)
+
+    r.incr(f"count:{url}")
+    return html
